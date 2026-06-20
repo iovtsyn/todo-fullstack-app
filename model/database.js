7adea1480d79ebfff6f9dcc9351e195a9598365f
Ilya Ovtsyn <114185658+iovtsyn@users.noreply.github.com>
2026-06-20 13:05:04 +0100

    Configure PostgreSQL SSL for Render

--- model/database.js
+++ model/database.js
@@ -3,10 +3,13 @@ require('dotenv').config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
